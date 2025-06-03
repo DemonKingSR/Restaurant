42cef448e2b101f910e6b8b62e7dccafcb4b6a18
DemonKingSR <shivankreddyninja@gmail.com>
2025-06-03 11:30:10 +0530

    added static

--- AlCapino/urls.py
+++ AlCapino/urls.py
@@ -21,3 +21,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('App1.urls')),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.conf import settings
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
